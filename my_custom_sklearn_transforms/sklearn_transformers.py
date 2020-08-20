47ffcc57c320c01a69632307e8a006ef0ce28c1e
robertodepaiva <69477091+robertodepaiva@users.noreply.github.com>
2020-08-19 23:00:49 -0300

    Update sklearn_transformers.py
    Importando...

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
+import numpy as numpy
+import pandas as pandas
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
